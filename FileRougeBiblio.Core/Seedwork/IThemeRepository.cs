dde506287b348194838e66ccc15f765a670e3afb
Saami PERDRIX <saami@perdrix.xyz>
2023-04-24 15:10:08 +0200

    Correction Theme

--- FileRougeBiblio.Core/Seedwork/IThemeRepository.cs
+++ FileRougeBiblio.Core/Seedwork/IThemeRepository.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FilRougeBiblio.Infrastructure.Data
+namespace FilRougeBiblio.Core.Seedwork
 {
     public interface IThemeRepository
     {
