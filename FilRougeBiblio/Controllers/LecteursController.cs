b6459c5e0b9fc12e43774326c670efc4f2c0bcac
Saami PERDRIX <saami@perdrix.xyz>
2023-04-25 16:24:49 +0200

    lecteur bool (ok à la création, ko à l'édition)

--- FilRougeBiblio/Controllers/LecteursController.cs
+++ FilRougeBiblio/Controllers/LecteursController.cs
@@ -87,7 +87,7 @@ namespace FilRougeBiblio.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Nom,Prenom,Email,Telephone,Adresse,Cotisation,Id")] Lecteur lecteur)
+        public async Task<IActionResult> Edit(int id, Lecteur lecteur)
         {
             if (id != lecteur.Id)
             {
