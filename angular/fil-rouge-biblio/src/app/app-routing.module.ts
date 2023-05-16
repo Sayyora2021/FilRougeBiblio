df6e7db0ca9656ca52213cb97db51763f9043170
Sayyora2021 <96926176+Sayyora2021@users.noreply.github.com>
2023-05-16 10:42:58 +0200

    upd-motclefs_done

--- angular/fil-rouge-biblio/src/app/app-routing.module.ts
+++ angular/fil-rouge-biblio/src/app/app-routing.module.ts
@@ -3,11 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { CreateLecteurComponent } from 'src/Components/Lecteur/create-lecteur/create-lecteur.component';
 import { DeleteLecteursComponent } from 'src/Components/Lecteur/delete-lecteurs/delete-lecteurs.component';
 import { ListLecteursComponent } from 'src/Components/Lecteur/list-lecteurs/list-lecteurs.component';
+import { CreateMotclefsComponent } from 'src/Components/MotClefs/create-motclefs/create-motclefs.component';
+
 
 const routes: Routes = [{
   path: 'Lecteurs/Create',
   component: CreateLecteurComponent
-},{
+},
+{
+path: 'MotClefs/Create',
+component: CreateMotclefsComponent
+},
+{
   path: 'Lecteurs',
   component: ListLecteursComponent
 },{
