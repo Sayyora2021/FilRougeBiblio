378886f831cb6c48bb92d4e38dc31092278b1f4d
Sayyora2021 <96926176+Sayyora2021@users.noreply.github.com>
2023-05-22 11:28:44 +0200

    test

--- FileRougeBiblio.Core/Entities/Auteur.cs
+++ FileRougeBiblio.Core/Entities/Auteur.cs
@@ -16,6 +16,6 @@ namespace FilRougeBiblio.Core.Entities
         [MaxLength(50)]
         public string Nom { get; set; } = null!;
 
-        public virtual List<Livre> Livres { get; set; } = null!;
+        public virtual List<Livre> Livres   { get; set; } = null!;
     }
 }
